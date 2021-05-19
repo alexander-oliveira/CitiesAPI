2d5c6b0df108ef26104ee39596c090746195f690
Alexander <alex_osilva@hotmail.com>
2021-05-19 19:11:23 -0300

    feat(State): add properties annotations

--- src/main/java/com/github/alexanderoliveira/citiesapi/states/State.java
+++ src/main/java/com/github/alexanderoliveira/citiesapi/states/State.java
@@ -1,15 +1,28 @@
 package com.github.alexanderoliveira.citiesapi.states;
 
 import com.github.alexanderoliveira.citiesapi.countries.Country;
-
+import org.hibernate.annotations.Table;
+import javax.persistence.*;
 import java.util.List;
 
+@Entity(name = "State")
+@Table(name = "estado")
 public class State {
+
+    @Id
     private Long id;
+
+    @Column(name = "nome")
     private String name;
+
     private String uf;
+
     private Integer ibge;
+
+    @ManyToOne
+    @JoinColumn(name = "pais", referencedColumnName = "id")
     private Country country;
+
     private List<Integer> ddd;
 
     public State() {
