11792d1e85fe6acedbc1583cd2d3af0abf783308
Alexander <alex_osilva@hotmail.com>
2021-05-19 17:01:40 -0300

    fix(GetCountriesById): check for out of bound id

--- src/main/java/com/github/alexanderoliveira/citiesapi/CountryResource.java
+++ src/main/java/com/github/alexanderoliveira/citiesapi/CountryResource.java
@@ -2,8 +2,10 @@ package com.github.alexanderoliveira.citiesapi;
 
 import com.github.alexanderoliveira.citiesapi.countries.Country;
 import com.github.alexanderoliveira.citiesapi.repository.CountryRepository;
+import org.apache.coyote.Response;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,8 +28,13 @@ public class CountryResource {
     }
 
     @GetMapping("/{id}")
-    public Country getOne(@PathVariable Long id){
+    public ResponseEntity getOne(@PathVariable Long id){
         Optional<Country> optional = repository.findById(id);
-        return optional.get();
+        if(optional.isPresent()){
+            return ResponseEntity.ok().body(optional.get());
+        }
+        else{
+            return ResponseEntity.notFound().build();
+        }
     }
 }
