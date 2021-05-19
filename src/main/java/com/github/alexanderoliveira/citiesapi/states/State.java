589b9bf1e2e3b7b88c4d3eba07e4796db871769f
Alexander <alex_osilva@hotmail.com>
2021-05-19 18:52:15 -0300

    feat(State): add State properties

--- src/main/java/com/github/alexanderoliveira/citiesapi/states/State.java
+++ src/main/java/com/github/alexanderoliveira/citiesapi/states/State.java
@@ -1,2 +1,17 @@
-package com.github.alexanderoliveira.citiesapi.states;public class State {
+package com.github.alexanderoliveira.citiesapi.states;
+
+import com.github.alexanderoliveira.citiesapi.countries.Country;
+
+import java.util.List;
+
+public class State {
+    private Long id;
+    private String name;
+    private String uf;
+    private Integer ibge;
+    private Country country;
+    private List<Integer> ddd;
+
+    public State() {
+    }
 }
